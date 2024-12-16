26d898f2616eccde268eafe8df1a9432ba8ea4cc
jigreda <jigreda@unsa.edu.pe>
2024-12-16 17:47:15 -0500

    Funcion para registrar un nuevo articulo

---
+++ doNew.js
@@ -0,0 +1,13 @@
+function doNew(){
+    // encodeURI reemplaza espacios con %20, entonces reemplazamos %20 con + para la url
+    let titulo = encodeURIComponent(document.getElementById("titulo").value).replace(/%20/g, "+");
+    let cuerpo = encodeURIComponent(document.getElementById("cuerpo").value).replace(/%20/g,"+");
+    let url = "http://localhost:8080/cgi-bin/new.pl?usuario="+userKey+"&titulo="+titulo+"&cuerpo="+cuerpo;
+    console.log(url);
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send();
+    xhr.onload = function () {
+        responseNew(xhr.responseXML);
+    };
+}
