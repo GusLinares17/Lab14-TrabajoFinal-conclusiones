d95cd5b2421e19ae45392de0abeeab01c748f420
jigreda <jigreda@unsa.edu.pe>
2024-12-16 13:39:01 -0500

    Correcciones en la propuesta del register

--- register.pl
+++ register.pl
@@ -7,16 +7,25 @@ use DBI;
 # Conexión a la base de datos
 my $dbh = DBI->connect('DBI:mysql:database=tu_base;host=localhost', 'usuario', 'contraseña', { RaiseError => 1, AutoCommit => 1 });
 
+# Obtener parámetros del formulario
 my $userName = param('userName');
 my $password = param('password');
 my $firstName = param('firstName');
 my $lastName = param('lastName');
 
+# Enviar encabezado XML
 print header(-type => 'application/xml', -charset => 'UTF-8');
 print "<?xml version='1.0' encoding='utf-8'?>\n";
 
-if ($userName && $password && $firstName && $lastName) {
+# Validación de parámetros
+if (defined $userName && defined $password && defined $firstName && defined $lastName &&
+    $userName =~ /^[a-zA-Z0-9_]+$/ && length($password) >= 6 && 
+    $firstName =~ /^[a-zA-Z]+$/ && $lastName =~ /^[a-zA-Z]+$/) {
+
+    # Preparar la consulta
     my $sth = $dbh->prepare("INSERT INTO Users (userName, password, firstName, lastName) VALUES (?, ?, ?, ?)");
+
+    # Ejecutar la consulta con manejo de errores
     eval {
         $sth->execute($userName, $password, $firstName, $lastName);
         print "<user>\n";
@@ -24,12 +33,16 @@ if ($userName && $password && $firstName && $lastName) {
         print "  <firstName>$firstName</firstName>\n";
         print "  <lastName>$lastName</lastName>\n";
         print "</user>\n";
-    } or do {
-        print "<user></user>\n";
     };
+    if ($@) { # Si ocurrió un error en la base de datos
+        print "<error>Database error: $@</error>\n";
+    }
+
     $sth->finish;
 } else {
-    print "<user></user>\n";
+    # Respuesta si los parámetros no son válidos
+    print "<error>Invalid input data</error>\n";
 }
 
+# Cerrar conexión a la base de datos
 $dbh->disconnect;
