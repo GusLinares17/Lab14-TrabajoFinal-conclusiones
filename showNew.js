c6793e2bf162d73724fca8b903af02baf6cacc4b
jigreda <jigreda@unsa.edu.pe>
2024-12-16 22:29:02 -0500

    Pequeñas correciones y mejoras al formulario

--- showNew.js
+++ showNew.js
@@ -1,9 +1,13 @@
-function showNew(){
-    let showNew = `<p>Titulo</p>
-    <input id='titulo' name ='titulo' type='text'><br>
-    <p>Contenido-markdown</p>
-    <textarea style = "width: 100%;" type="text" id ="cuerpo" name="cuerpo"></textarea><br>
-    <button onclick='doNew()'>Enviar</button>
-    <button onclick='doList()'>Cancelar</button>`;
+function showNew() {
+    let showNew = `
+        <form>
+            <p><label for="titulo">Título</label></p>
+            <input id="titulo" name="titulo" type="text"><br>
+            <p><label for="cuerpo">Contenido (Markdown)</label></p>
+            <textarea id="cuerpo" name="cuerpo" style="width: 100%;"></textarea><br>
+            <button type="button" onclick="doNew()">Enviar</button>
+            <button type="button" onclick="doList()">Cancelar</button>
+        </form>
+    `;
     document.getElementById("main").innerHTML = showNew;
 }
