21ad6dac4c25b3b041f557d4154ed3281946b6b3
jigreda <jigreda@unsa.edu.pe>
2024-12-16 23:08:50 -0500

    Version final para crear los articulos

--- doNew.js
+++ doNew.js
@@ -1,21 +1,13 @@
-function doNew() {
-    let titulo = encodeURIComponent(document.getElementById("titulo").value).replace(/%20/g, "+");
-    let cuerpo = encodeURIComponent(document.getElementById("cuerpo").value).replace(/%20/g, "+");
-    let url = `http://localhost:8080/cgi-bin/new.pl?usuario=${userKey}&titulo=${titulo}&cuerpo=${cuerpo}`;
-    
+function doNew(){
+    // encodeURI reemplaza espacios con %20, entonces reemplazamos %20 con + para la url
+    let titulo = encodeURIComponent(document.getElementById("titulo").value).replace(/%20/g, "+"); // para reemplazar espacios en el url
+    let cuerpo = encodeURIComponent(document.getElementById("cuerpo").value).replace(/%20/g,"+");
+    let url = "http://localhost:8080/cgi-bin/new.pl?usuario="+userKey+"&titulo="+titulo+"&cuerpo="+cuerpo;
     console.log(url);
-
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.text(); // Si la respuesta no es XML, ajusta esto
-        })
-        .then(responseText => {
-            responseNew(responseText); // o responseXML si es necesario
-        })
-        .catch(error => {
-            console.error("Error en la solicitud:", error);
-        });
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.send();
+    xhr.onload = function () {
+        responseNew(xhr.responseXML);
+    };
 }
